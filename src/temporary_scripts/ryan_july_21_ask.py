6fd62679959b7cb73aec514da680a4f66ed104e7
aarav <aaravzen@gmail.com>
2024-02-09 10:24:51 -0500

    updated ask from ryan for out of date dominant projects

--- src/temporary_scripts/ryan_july_21_ask.py
+++ src/temporary_scripts/ryan_july_21_ask.py
@@ -1,8 +1,7 @@
 '''
         print("Queue Number,Latitute,Longitude,Queue Date,Interdependency Status")
 
-        
-        
+
                     if fn == "2024_01_31.csv":
                         if proj.interdependency_status == "Project A" or proj.interdependency_status == "Project B":
                             qd = proj.queue_date
@@ -13,4 +12,21 @@
                                 print(f'{proj.queue_number}, {proj.lat}, {proj.long}, {proj.queue_date}, {proj.interdependency_status}')
                             elif y < 21:
                                 print(f'{proj.queue_number}, {proj.lat}, {proj.long}, {proj.queue_date}, {proj.interdependency_status}')
+'''
+
+
+'''
+        print("Queue Number,Latitute,Longitude,Queue Date,Substation Name,Interdependency Status")
+
+        
+                    if fn == "2024_01_31.csv":
+                        if proj.interdependency_status == "Project A" or proj.interdependency_status == "Project B":
+                            qd = proj.queue_date
+                            m,d,y = qd.split("/")
+                            y = int(y)
+                            m = int(m)
+                            if y <= 21 and m <= 7:
+                                print(f'{proj.queue_number}, {proj.lat}, {proj.long}, {proj.queue_date}, {p["Substation Name"]}, {proj.interdependency_status}')
+                            elif y < 21:
+                                print(f'{proj.queue_number}, {proj.lat}, {proj.long}, {proj.queue_date}, {p["Substation Name"]}, {proj.interdependency_status}')
 '''
