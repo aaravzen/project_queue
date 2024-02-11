8fca216636e14eaf4b5ed72b454e3348fdce023d
aarav <aaravzen@gmail.com>
2024-02-11 09:13:07 -0500

    generating duplicates by quarter

--- src/temporary_scripts/duplicate_projects.py
+++ src/temporary_scripts/duplicate_projects.py
@@ -1,4 +1,4 @@
-from dominion_data import DominionData, Substation
+from dominion_data import DominionData
 
 if __name__ == "__main__":
     print("Queue Number,Latitute,Longitude,Queue Date,Substation Name,Substation Transformer,Capacity (MW),Interdependency Status")
@@ -6,17 +6,25 @@ if __name__ == "__main__":
 
     data = DominionData()
     snames = sorted(data.get_substation_names())
+    printers = {}
     for sname in snames:
         sub = data.get_substation(sname)
         groupings = sub.get_groupings()
         for grouping in groupings:
+            y,q,t = grouping
             transf = sub.projects[grouping]
             dominants = transf.get_dominant_projects()
             if dominants:
                 for qn,l in dominants:
-                    if qn in printed:
-                        continue
-                    print(l)
+                    # if qn in printed:
+                    #     continue
+                    if (y,q) not in printers:
+                        printers[(y,q)] = []
+                    printers[(y,q)].append(l)
                     printed.add(qn)
-        # if printed:
-        #     print("")
+    
+    for printer in sorted(printers.keys()):
+        print(f"{printer}")
+        print(f"{len(printers[printer])}")
+        # for line in printers[printer]:
+        #     print(line)
